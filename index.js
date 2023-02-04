19b884b0f3c0321dd08867b62f5ece6aa601a919
Bruno <101802601+nobrupsiq@users.noreply.github.com>
2023-02-04 11:32:47 -0300

    Update index 🔨

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ console.log('Digite as propriedades CSS ou digite "SAIR" para finalizar.');
 
 // Processamento dos dados
 // Se o valor que o usuario digitar for diferente de 'sair', irá continuar pedindo pro usuario digitar propriedade no input. Quando o usuario digitar 'sair', o looping então é encerrado e apresenta a saida de dados com as propriedades que o usuário havia digitado
+// toLowerCase garante que o 'sair' do usuario seja sempre em letras de caixa baixa, independente se for digitado 'SAIR'
 while (input.toLowerCase() !== 'sair') {
   input = readlineSync.question('Digite a propriedade: ');
   if (input.toLowerCase() !== 'sair') {
